784edab24ef8995d774a7907e153a9f1a53adca7
newdevmike22 <paola@dominicansoul.com>
2026-08-04 14:32:22 -0400

    Setup and styled the checkout page.
    Setup and styled the checkout page for the ecommerce app.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import HomePage from "./components/HomePage";
 import { Routes, Route } from "react-router";
+import HomePage from "./components/HomePage";
+import CheckoutPage from "./components/CheckoutPage";
 
 const App = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>Test checkout page</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 };
