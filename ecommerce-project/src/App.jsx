699d31029edec1810034cb955a2a9c31e78156a2
newdevmike22 <paola@dominicansoul.com>
2026-08-04 14:58:18 -0400

    Added the orders page to the app
    Added the orders page to the ecommerce app.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 import HomePage from "./components/HomePage";
 import CheckoutPage from "./components/CheckoutPage";
+import Orders from "./components/Orders";
 
 const App = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<Orders />} />
     </Routes>
   );
 };
