e0894a7c9bc16587e794ade72e0bcac01bd95922
newdevmike22 <paola@dominicansoul.com>
2026-08-05 11:11:42 -0400

    Generate HTML for the cart items
    Generated HTML for the cart items.

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./HomePage.css";
 import Header from "../components/Header";
+import { formatMoney } from "../utils/money";
 
 const HomePage = ({ cart }) => {
   const [products, setProducts] = useState([]);
@@ -33,7 +34,7 @@ const HomePage = ({ cart }) => {
                   <div className="product-rating-count link-primary">{product.rating.count}</div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">{formatMoney(product.priceCents)}</div>
 
                 <div className="product-quantity-container">
                   <select>
