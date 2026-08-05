ab56e27ce81fab100a700fdbba6d4d0e6a4d083a
newdevmike22 <paola@dominicansoul.com>
2026-08-05 15:10:58 -0400

    Split part of HomePage.jsx into a new component
    Split part of HomePage.jsx into a new component.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Routes, Route } from "react-router";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/home/HomePage";
 import CheckoutPage from "./pages/CheckoutPage";
 import Orders from "./pages/Orders";
 import Tracking from "./pages/Tracking";
