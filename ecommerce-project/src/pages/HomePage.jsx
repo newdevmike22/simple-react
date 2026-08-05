aaefcc9cf41c428b50d0276f69cc8a38b823fe7b
newdevmike22 <paola@dominicansoul.com>
2026-08-05 09:42:53 -0400

    Used Axios to fetch the data
    Used Axios to fetch the data from the backend.

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
+import axios from "axios";
 import "./HomePage.css";
 import Header from "../components/Header";
 import { products } from "./../../starting-code/data/products";
 
 const HomePage = () => {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
